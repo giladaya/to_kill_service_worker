e4276b4fc90269a0881d63ec9dea72b339962c8e
giladd <giladd@trainologic.com>
2019-08-14 17:44:17 +0300

    replace self-destruct code

--- src/sw04.destruct.js
+++ src/sw04.destruct.js
@@ -1,40 +1,24 @@
-// source: https://novemberfive.co/blog/mess-up-service-workers-caching-gotcha
-// also see: https://github.com/NekR/self-destroying-sw
+// source: https://github.com/NekR/self-destroying-sw
+// also see: https://novemberfive.co/blog/mess-up-service-workers-caching-gotcha
 // remember to remove registration code!
 
-const VER = '0.4.1';
+const VER = "0.4.1";
 console.log(`Hello from service worker v${VER}`);
 
-const DIRTY_CACHE_NAME = "sw-cache";
-
-self.addEventListener("install", () => {
-  // Skip over the "waiting" lifecycle state, to ensure that our
-  // new service worker is activated immediately, even if there's
-  // another tab open controlled by our older service worker code.
+self.addEventListener("install", function(e) {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  // Unregister self
-  self.registration.unregister().then(() => console.log('Unregistered'));
-  // Now clear caches
-  event.waitUntil(
-    caches
-      .keys()
-      .then(cacheNames =>
-        // caches are shared across whole origin
-        cacheNames.filter(
-          cacheName => cacheName.indexOf(DIRTY_CACHE_NAME) !== -1
-        )
-      )
-      .then(cachesToDelete =>
-        Promise.all(
-          cachesToDelete.map(cacheToDelete => {
-            console.log("Deleting cache", cacheToDelete);
-            return caches.delete(cacheToDelete);
-          })
-        )
-      )
-      .then(() => self.clients.claim())
-  );
+self.addEventListener("activate", function(e) {
+  self.registration
+    .unregister()
+    .then(function() {
+      console.log("Unregistered");
+      // TODO: cleanup
+      return self.clients.matchAll();
+    })
+    .then(function(clients) {
+      console.log("Reloading clients");
+      clients.forEach(client => client.navigate(client.url));
+    });
 });
